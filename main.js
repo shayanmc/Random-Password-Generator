58db0d6931aac540eb68591a9e2d9f1d5dd17a24
shayan <shayan.133331@gmail.com>
2023-06-20 22:40:57 +0330

    add js

--- main.js
+++ main.js
@@ -9,23 +9,31 @@ btnEl.addEventListener("click", () => {
 
 copyIcon.addEventListener("click", () => {
   copyPassword();
-  alertContainerEl.classList.remove("active")
+  alertContainerEl.classList.remove("active");
   setTimeout(() => {
-    alertContainerEl.classList.remove("active")
+    alertContainerEl.classList.remove("active");
   }, 2000);
 });
 
 function createPassword() {
-
   const chars =
     "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  
-    const passwordLength = 13;
-  
-    let password = "";
 
-  
-    for (let index = 0; index < passwordLength; index++) {
+  const passwordLength = 13;
+
+  let password = "";
+
+  for (let index = 0; index < passwordLength; index++) {
     const randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
-    }
+  }
+
+  inputEl.value = password;
+}
+
+function copyPassword() {
+  inputEl.select();
+  inputEl.setSelectionRange(0, 9999);
+  navigator.clipboard.writeText(inputEl.value);
+  alertContainerEl.innerText = password + "copied";
+}
