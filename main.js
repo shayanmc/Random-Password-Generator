28e707f49bfbad21b420d7ac971a07945ae45068
shayan <shayan.133331@gmail.com>
2023-06-20 22:33:31 +0330

    add js file

---
+++ main.js
@@ -0,0 +1,31 @@
+const btnEl = document.querySelector(".btn");
+const inputEl = document.querySelector("#input");
+const copyIcon = document.querySelector(".fa-copy");
+const alertContainerEl = document.querySelector(".alert-container");
+
+btnEl.addEventListener("click", () => {
+  createPassword();
+});
+
+copyIcon.addEventListener("click", () => {
+  copyPassword();
+  alertContainerEl.classList.remove("active")
+  setTimeout(() => {
+    alertContainerEl.classList.remove("active")
+  }, 2000);
+});
+
+function createPassword() {
+
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+    const passwordLength = 13;
+  
+    let password = "";
+
+  
+    for (let index = 0; index < passwordLength; index++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+    }
